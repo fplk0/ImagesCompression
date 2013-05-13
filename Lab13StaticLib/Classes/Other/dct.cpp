d17155465ba1cc08ba1fd0e16e1055f13641cb5d
Sfairat <sfairat.od@gmail.com>
2013-05-13 21:14:34 +0300

    DCT was improved (inlining it was made possible).

--- Lab13StaticLib/Classes/Other/dct.cpp
+++ Lab13StaticLib/Classes/Other/dct.cpp
@@ -3,8 +3,12 @@
 #include <cmath>
 #include "Atomic.h"
 
-static float dctMatrix[8][8];
-static float idctMatrix[8][8];
+#define __DEFINE_DCT_EXTERN__
+
+#include "dct.h"
+
+#undef __DEFINE_DCT_EXTERN__
+
 static const double PI = acos(-1.0);
 
 void initDCT()
@@ -15,40 +19,14 @@ void initDCT()
 		{
 			for (int k = 0; k < 8; k++)
 			{
-				dctMatrix[j][k] = 0.5f * cos(PI / 8 * (k + 0.5f) * j);
-				idctMatrix[j][k] = 0.5f * cos(PI / 8 * (j + 0.5f) * k);
+				SFdctMatrix[j][k] = 0.5f * cos(PI / 8 * (k + 0.5f) * j);
+				SFidctMatrix[j][k] = 0.5f * cos(PI / 8 * (j + 0.5f) * k);
 				if (j == 0)
-					dctMatrix[j][k] *= 1 / sqrt(2.0f);
+					SFdctMatrix[j][k] *= 1 / sqrt(2.0f);
 				if (k == 0)
-					idctMatrix[j][k] *= 1 / sqrt(2.0f);
+					SFidctMatrix[j][k] *= 1 / sqrt(2.0f);
 			}
 		}
 	}
 	SFPerformOnceEnd
-}
-
-void perform1DDCT(float src[8], float dst[8], bool inv)
-{
-	float (*dctMatr)[8];
-	if (inv)
-		dctMatr = idctMatrix;
-	else
-		dctMatr = dctMatrix;
-	for (int j = 0; j < 8; j++)
-	{
-		float sum = 0;
-		for (int k = 0; k < 8; k++)
-			sum += src[k] * dctMatr[j][k];
-		dst[j] = sum;
-	}
-}
-
-void perform1DDCT(float src[8], float dst[8])
-{
-	perform1DDCT(src, dst, false);
-}
-
-void perform1DIDCT(float src[8], float dst[8])
-{
-	perform1DDCT(src, dst, true);
 }
--- Lab13StaticLib/Classes/Other/dct.h
+++ Lab13StaticLib/Classes/Other/dct.h
@@ -1,6 +1,42 @@
 #pragma once
 
+#undef __SF_DCT_EXTERN__
+
+#ifdef __DEFINE_DCT_EXTERN__
+#define __SF_DCT_EXTERN__
+#else
+#define __SF_DCT_EXTERN__ extern
+#endif
+
+__SF_DCT_EXTERN__ float SFdctMatrix[8][8];
+__SF_DCT_EXTERN__ float SFidctMatrix[8][8];
+
 void initDCT();
 
-void perform1DDCT(float src[8], float dst[8]);
-void perform1DIDCT(float src[8], float dst[8]);
+inline void perform1DDCT(float src[8], float dst[8], bool inv)
+{
+	float (*dctMatr)[8];
+	if (inv)
+		dctMatr = SFidctMatrix;
+	else
+		dctMatr = SFdctMatrix;
+	for (int j = 0; j < 8; j++)
+	{
+		float sum = 0;
+		for (int k = 0; k < 8; k++)
+			sum += src[k] * dctMatr[j][k];
+		dst[j] = sum;
+	}
+}
+
+inline void perform1DDCT(float src[8], float dst[8])
+{
+	perform1DDCT(src, dst, false);
+}
+
+inline void perform1DIDCT(float src[8], float dst[8])
+{
+	perform1DDCT(src, dst, true);
+}
+
+#undef __SF_DCT_EXTERN__
